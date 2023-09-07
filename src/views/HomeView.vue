0c62fd7fec3067adb88b7013e07fe5e7cf4b3610
zhanglianrun <1064732420@qq.com>
2023-09-07 15:23:47 +0800

    可以调整字颜色

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,12 @@
   <div style="height: 100%">
     <el-container style="height: 100%; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246);height: 100%">
-        <el-menu :default-openeds="['1', '3']"  style="height: 100%" background-color="rgb(48,65,86)">
+        <el-menu :default-openeds="['1', '3']"  style="height: 100%"
+                 background-color="rgb(48,65,86)"
+                 text-color="#fff"
+                 active-text-color="#ffd04b"
+
+        >
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-message"></i>导航一</template>
             <el-menu-item-group>
